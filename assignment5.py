72a2768cdaeef97b7773534d3ee21d5be2165099
Mewiiarts <mewiimella05@gmail.com>
2024-03-28 13:44:07 -0700

    hw

---
+++ assignment5.py
@@ -0,0 +1,11 @@
+car = {
+ "Name": "Supra",
+ "Manufacture date": "2022-02-14",
+ "Speed": "200km/hr",
+ "Color": "blue"
+}
+
+print(car["Name"])
+print(car["Manufacture date"])
+print(car["Speed"])
+print(car["Color"])
